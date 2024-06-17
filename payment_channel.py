bf58b81c8088a250cd949911f237285fe7b98d3d
logicalmechanism <logical.mechanism@protonmail.com>
2024-06-17 02:08:39 -0700

    use merkly as the data storage for the payment channel update

--- payment_channel.py
+++ payment_channel.py
@@ -2,3 +2,5 @@
 # identified by their registry. A user will send funds to another user over the
 # L2 and will produce a proof that allows the L2 to be updated then the L2 will
 # rollup all the transactions onto L1.
+
+# use merkly as the datum
