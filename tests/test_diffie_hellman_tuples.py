5d500dd788388bf10779f7abb99987517f6418ff
logic <logical.mechanism@protonmail.com>
2024-06-24 21:35:33 -0700

    adding tests to dht

--- tests/test_diffie_hellman_tuples.py
+++ tests/test_diffie_hellman_tuples.py
@@ -22,12 +22,36 @@ def test_self_prove_DHTuple_is_false():
     assert outcome is False
 
 
+def test_twice_rerandomized_is_false():
+    alice = Registry()
+    alice_random = copy.deepcopy(alice)
+    alice.rerandomize()
+    y = alice.rng()
+    alice_random.rerandomize(y)
+    outcome = proveDHTuple(y, alice, alice_random)
+    assert outcome is False
+
+
 def test_mix_prove_DHTuple_is_false():
     alice = Registry()
     bob = Registry()
     y = alice.rng()
     outcome = proveDHTuple(y, alice, bob)
     assert outcome is False
+
+
+def test_sigma_statement():
+    alice = Registry()
+    bob = Registry()
+    y = alice.rng()
+    alice_random = copy.deepcopy(alice)
+    alice_random.rerandomize(y)
+    bob_random = copy.deepcopy(bob)
+    bob_random.rerandomize(y)
+    alice_outcome = proveDHTuple(y, alice, alice_random) or proveDHTuple(y, alice, bob_random)
+    bob_outcome = proveDHTuple(y, bob, alice_random) or proveDHTuple(y, bob, bob_random)
+    assert alice_outcome
+    assert bob_outcome
 
 
 if __name__ == "__main__":
