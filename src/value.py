de5b72abe4bd22611b17742abc72fbabcfa439c7
logic <logical.mechanism@protonmail.com>
2024-08-11 20:14:15 -0700

    need addition subtraction and scaler multiplication

--- src/value.py
+++ src/value.py
@@ -29,9 +29,41 @@ class Value:
         self._remove_zero_entries()
         return Value(self.inner)
 
+    def __sub__(self, other):
+        if not isinstance(other, Value):
+            return NotImplemented
+        for policy, assets in other.inner.items():
+            if policy in self.inner:
+                for asset, quantity in assets.items():
+                    if asset in self.inner[policy]:
+                        self.inner[policy][asset] -= quantity
+                    else:
+                        self.inner[policy][asset] = -quantity
+            else:
+                self.inner[policy] = {}
+                for asset, quantity in assets.items():
+                    self.inner[policy][asset] = -quantity
+
+        self._remove_zero_entries()
+        return Value(self.inner)
+
+    def __mul__(self, scale):
+        if not isinstance(scale, int):
+            return NotImplemented
+        if scale == 0:
+            Value({})
+        new_inner = {}
+        for policy, assets in self.inner.items():
+            new_assets = {asset: quantity * scale for asset, quantity in assets.items()}
+            new_inner[policy] = new_assets
+        return Value(new_inner)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
     def negate(self):
         """
-        All quantities are multiplied by -1
+        All quantities in self are multiplied by -1.
         """
         for policy, assets in self.inner.items():
             for asset, quantity in assets.items():
