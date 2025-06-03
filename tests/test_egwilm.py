21cdf6c3d34bfc152e4e5a92f643ab013a5d3712
logicalmechanism <logicalmechanism@protonmail.com>
2025-06-03 16:03:51 -0700

    adding a bob cant extract test

--- tests/test_egwilm.py
+++ tests/test_egwilm.py
@@ -7,6 +7,7 @@ import string
 from src.Registry import Registry
 from src.reversible_mapping import map_to_point, string_to_int
 
+
 def test_simple_egwilm():
     alice = Registry()
 
@@ -25,6 +26,7 @@ def test_simple_egwilm():
     assert alice_reverse_mapping_sig.prove(alice_reverse_mapping_sig.c1 * alice.x)
     assert message == msg
 
+
 def test_message_too_long1():
     alice = Registry()
     msg = "This message will be too long to encrypt with egwilm."
@@ -32,24 +34,30 @@ def test_message_too_long1():
     with pytest.raises(ValueError, match="Data too large to fit in field"):
         alice.reverse_mapping_encryption(msg)
 
+
 def test_message_too_long2():
-    alice = Registry()
     msg = "This message will be too long for encryption pr"
+    # purposely make it 1 byte less than the field modulus length
     # 1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab
     # 54686973206d6573736167652077696c6c20626520746f6f206c6f6e6720666f7220656e6372797074696f6e207072
-    print(msg.encode('utf-8').hex())
+    print(msg.encode("utf-8").hex())
+
 
 def test_random_message():
     alice = Registry()
     # run 100 tests and find some large off set for base64 string of length 47
     offsets = []
     for _ in range(1000):
-        msg = ''.join(random.choices(string.ascii_letters + string.digits + '+' + '/', k=47))
+        msg = "".join(
+            random.choices(string.ascii_letters + string.digits + "+" + "/", k=47)
+        )
         point, offset = map_to_point(msg)
         offsets.append(offset)
         # print(msg, offset)
     print(min(offsets), max(offsets))
-    msg = ''.join(random.choices(string.ascii_letters + string.digits + '+' + '/', k=47))
+    msg = "".join(
+        random.choices(string.ascii_letters + string.digits + "+" + "/", k=47)
+    )
     alice_reverse_mapping_sig = alice.reverse_mapping_encryption(msg)
     message = alice_reverse_mapping_sig.extract(alice_reverse_mapping_sig.c1 * alice.x)
     print(message)
@@ -57,4 +65,18 @@ def test_random_message():
     assert alice_reverse_mapping_sig.prove(alice_reverse_mapping_sig.c1 * alice.x)
     assert message == msg
 
+def test_bob_cant_extract_or_decrypt():
+    alice = Registry()
+    bob = Registry()
+
+    msg = "".join(
+        random.choices(string.ascii_letters + string.digits + "+" + "/", k=47)
+    )
+    alice_reverse_mapping_sig = alice.reverse_mapping_encryption(msg)
+
+    with pytest.raises(ValueError):
+        alice_reverse_mapping_sig.extract(alice_reverse_mapping_sig.c1 * bob.x)
+    
+    with pytest.raises(UnicodeDecodeError):
+        alice_reverse_mapping_sig.prove(alice_reverse_mapping_sig.c1 * bob.x)
 
